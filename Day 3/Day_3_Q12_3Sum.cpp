55b04620395303878dcb22da88f49fc0f1c838e2
Raghuraj Thombare <65705348+IamRaghuraj@users.noreply.github.com>
2022-03-26 22:11:54 +0530

    day 3 last problem

--- Day 3/Day_3_Q12_3Sum.cpp
+++ Day 3/Day_3_Q12_3Sum.cpp
@@ -1,2 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        map<tuple<int,int,int>,int>mp;
+        if(nums.size()<3)
+            return ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            int temp = 0 - nums[i];
+            int j=i+1,k=nums.size()-1;
+            while(j<k){
+                if(nums[j]+nums[k] == temp){
+                    mp[make_tuple(nums[i],nums[j],nums[k])]++;
+                }
+                if(nums[j]+nums[k] > temp)
+                    k--;
+                else if(nums[j]+nums[k] < temp)
+                    j++;
+                else
+                    k--;
+            }
+        }
+        for(auto x:mp){
+            auto t = x.first;
+            ans.push_back({get<0>(t),get<1>(t),get<2>(t)});
+        }
+        return ans;
+    }
+};
